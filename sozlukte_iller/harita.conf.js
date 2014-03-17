35e61a63766fea695414c88191e1c709bb73bd99
Onur Derin <oderin@users.sourceforge.net>
2014-03-17 15:44:18 +0100

    ilk libviz denemesi eklendi.

--- sozlukte_iller/harita.conf.js
+++ sozlukte_iller/harita.conf.js
@@ -1,6 +1,6 @@
 window.conf_harita = {
- 'baslik': 'İllere göre sonuçlar',
- 'altbaslik': '(İl başlığındaki girdi sayısı / il nüfusu)x10<sup>6</sup>',
+ 'baslik': 'İl başlıklarındaki girdi yoğunluğu',
+ 'altbaslik': '(İl başlığındaki girdi sayısı / il nüfusu)x10<sup>3</sup>',
  'auto_dereceler': true,
  'dereceler': [0, 100, 250, 500, 1000, 4000, 8000],
  'yerel': false,
