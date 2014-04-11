83145d234851fef399569b78fe41266c38f9b8aa
Onur Derin <oderin@users.sourceforge.net>
2014-04-11 15:26:15 +0200

    istanbul\'a goc incelemesi eklendi.

--- istanbula-goc/harita.conf.js
+++ istanbula-goc/harita.conf.js
@@ -1,10 +1,10 @@
 window.conf_harita = {
- 'baslik': 'İl başlıklarındaki girdi yoğunluğu',
- 'altbaslik': '(İl başlığındaki girdi sayısı / il nüfusu)x10<sup>3</sup>',
+ 'baslik': 'İstanbul\'da hangi ilden kaç kişi yaşıyor?',
+ 'altbaslik': 'yuzdelik oran = 100 x (ilden istanbul\'a göç sayısı/il nüfusu)',
  'auto_dereceler': true,
  'dereceler': [0, 100, 250, 500, 1000, 4000, 8000],
  'yerel': false,
- 'csvDosyasi': 'http://derino.github.io/ssgpp_workspace/sozlukte_iller/sehir_girdi_sayilari.csv',
+ 'csvDosyasi': 'http://derino.github.io/ssgpp_workspace/istanbula-goc/goc_sayisi.csv',
 // yakinlastirma derecesi: [1, 8] araliginda bir tam sayi. 
 // ulkeler icin
 //'yakinlik': 2,
